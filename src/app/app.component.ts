e5e01a7d5d776042083846ee000635e765c771e3
Mark McNulty <markmcnulty84@hotmail.com>
2023-04-26 17:30:06 +0100

    Latest changes to Angular app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { Post } from './posts/post.model';
 // import { PostCreateComponent } from './posts/post-create/post-create.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'mean-course';
+  // storedPosts: Post[] = [];
+  // onPostAdded(post) {
+  //   this.storedPosts.push(post);
+  // }
 }
--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,17 +1,23 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+import { PostsService } from '../posts.service';
 
 @Component({
-    selector:'app-post-create',
-    templateUrl: './post-create.component.html'
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css'],
 })
 export class PostCreateComponent {
-    newPost="No Content";
-    enteredValue='';
+  newPost = 'No Content';
+  enteredTitle = '';
+  enteredContent = '';
 
-    onAddPost(){
-        
-        this.newPost=this.enteredValue;
-        
-    }
+  constructor(public postsService: PostsService) {}
 
+  onAddPost(form: NgForm) {
+    if (form.invalid) return;
+
+    this.postsService.addPost(form.value.title, form.value.content);
+  }
 }
